74418ccd871626ae1335187e8fb986f01d782614
VeLoct84 <unlimited84man@gmail.com>
2021-05-03 09:08:59 +0800

    App-05
    modefied code

--- App-5-Flatmates-Bill-Web-App/main.py
+++ App-5-Flatmates-Bill-Web-App/main.py
@@ -6,46 +6,67 @@ from flatmates_bill import flat
 
 app = Flask(__name__)
 
-class HomePage(MethodView):
 
+class HomePage(MethodView):
     def get(self):
-        return render_template('index.html')
+        # render the main Homepage with class flask.render_template()
+        return render_template("index.html")
 
 
 class BillFormPage(MethodView):
-    
     def get(self):
+        # refer class BillForm()
         bill_form = BillForm()
-        return render_template('bill_form_page.html',
-                               billform = bill_form)
+        # render the BillFormPage with class flask.render_template()
+        return render_template("bill_form_page.html", billform=bill_form)
 
 
 class ResultsPage(MethodView):
-
+    # POST request function
     def post(self):
+        # BillForm with child class flask.request.form() to extract data
         billform = BillForm(request.form)
-        amount = billform.amount.data
-        return amount
+
+        # refer flat.py class
+        the_bill = flat.Bill(float(billform.amount.data), billform.period.data)
+        # assign variable for flatmate1 and flamate2
+        flatmate1 = flat.Flatmate(
+            billform.name1.data, float(billform.days_in_house1.data)
+        )
+        flatmate2 = flat.Flatmate(
+            billform.name2.data, float(billform.days_in_house2.data)
+        )
+
+        return render_template(
+            "results.html",
+            name1=flatmate1.name,
+            amount1=flatmate1.pays(the_bill, flatmate2),
+            name2=flatmate2.name,
+            amount2=flatmate2.pays(the_bill, flatmate1),
+        )
 
 
 class BillForm(Form):
-    amount = StringField("Bill Amount: ")
-    period = StringField("Bill Period: ")
+    # properties interface using StringField() class
+    amount = StringField("Bill Amount: ", default="100")
+    period = StringField("Bill Period: ", default="April 2021")
 
-    name1 = StringField("Name: ")
-    days_in_house1 = StringField("Days in the house: ")
+    # ask user1 to keyin data
+    name1 = StringField("Name: ", default="Mardhiah")
+    days_in_house1 = StringField("Days in the house: ", default="20")
 
-    name2 = StringField("Name: ")
-    days_in_house2 = StringField("Days in the house: ")
+    # ask user2 to keyin data
+    name2 = StringField("Name: ", default="Abdul Muiz")
+    days_in_house2 = StringField("Days in the house: ", default="12")
 
-    button = SubmitField('Calculate')
+    # add button refer to ResultPage()
+    button = SubmitField("Result")
 
-app.add_url_rule('/', 
-                 view_func=HomePage.as_view('home_page'))
-app.add_url_rule('/bill_form', 
-                 view_func=BillFormPage.as_view('bill_form_page'))
-app.add_url_rule('/results', 
-                 view_func=BillFormPage.as_view('results_page'))
+
+# Flask.add_url_rule() refer to class
+app.add_url_rule("/", view_func=HomePage.as_view("home_page"))
+app.add_url_rule("/bill_form", view_func=BillFormPage.as_view("bill_form_page"))
+app.add_url_rule("/results", view_func=ResultsPage.as_view("results_page"))
 
 
 app.run(debug=True)
